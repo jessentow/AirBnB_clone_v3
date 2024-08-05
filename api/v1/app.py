59325fecdc560a23879163b53596d386b6d0da31
kweku-annan <kwekuannan.dev@gmail.com>
2024-08-05 23:52:21 +0000

    Commiting to create another branch

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,10 @@ def close_storage(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+
+
 if __name__ == "__main__":
     from os import getenv
     # Fetch host and port from environment variables or use defaults
